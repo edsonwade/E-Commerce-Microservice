bc716053ee01c0dc58ca6ee714828c02d3aea391
edsonwade <vanilsonmuhongo@gamil.com>
2024-04-19 21:22:49 +0100

    Initial commit

---
+++ department-service/src/main/java/net/javaguides/departmentservice/service/impl/DepartmentNotFoundException.java
@@ -0,0 +1,12 @@
+package net.javaguides.departmentservice.service.impl;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class DepartmentNotFoundException extends RuntimeException {
+
+    public DepartmentNotFoundException(String message) {
+        super(message);
+    }
+}
